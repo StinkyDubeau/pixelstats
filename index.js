06d431b41a71daf619867f02c79cc2a66c798dd5
StinkyDubeau <jakersunderscore09@gmail.com>
2023-12-23 07:19:10 -0500

    Add key handler function. Update .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import fs from "node:fs";
 
 const APP  = express();
 const PORT = 3000;
+const KEYPATH = "apikeys.txt";
+const KEY = getKey();
+
+function getKey(){ // Returns text from KEYPATH. Creates KEYPATH if it doesn't exist.
+    var key;
+    var needKeyMsg = '- You have not inserted your API key into apikeys.txt. The app will not work until you do. -'
+    try {
+        const data = fs.readFileSync(KEYPATH, 'utf8');
+        console.log(data);
+        key = data;
+      }
+      catch (err) {
+        try{
+            fs.writeFile(KEYPATH, needKeyMsg, (err) => {
+                if (err) throw err;
+                console.log(`${KEYPATH} has been created. Please paste your API key inside.`);
+              });
+        } catch(err){
+            console.error(`Create ${KEYPATH} and paste your API key inside.`);
+        }
+      }
+    return key;
+}
 
 APP.use(express.static("./public"));
 
@@ -12,5 +36,5 @@ APP.get("/", (req, res) => {
 });
 
 APP.listen(PORT, (req, res) => {
-    console.log("App is running on port " + PORT)
+    console.log(`App is running on port ${PORT}`)
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 Requirements:
 - npm
+
+First use:
+- The CLI should log your API keys at startup.
+
+Todo:
+- Auto create apikeys.txt if it doesn't exist.

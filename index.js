8ad6fe6411f052b823e8cb39efcbd1a9c9654a7c
devloves <65783463+devloves@users.noreply.github.com>
2023-12-28 18:27:53 +0530

    Fixed Spaces

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import bodyParser from "body-parser";
 import axios from "axios";
 import fs from "node:fs";
 
-const PROJECTNAME = "PixelStats"
-const APP  = express();
+const PROJECTNAME = "PixelStats";
+const APP = express();
 const PORT = 3000;
 const KEYPATH = "apikeys.txt";
 
@@ -15,17 +15,17 @@ const DEFAULTUUID = "f84c6a790a4e45e0879bcd49ebd4c4e2"; // This acts only as a f
 
 // Function to generate API configs dynamically
 function genAPIConfig(apiName, apiKey) {
-  const headers = {
-    "user-agent": `github.com/stinkydubeau/${PROJECTNAME}`,
-  };
+    const headers = {
+        "user-agent": `github.com/stinkydubeau/${PROJECTNAME}`,
+    };
 
-  if (apiKey) {
-    headers["API-key"] = apiKey;
-  }
+    if (apiKey) {
+        headers["API-key"] = apiKey;
+    }
 
-  return {
-    headers: headers,
-  };
+    return {
+        headers: headers,
+    };
 }
 
 const PLAYER_DB_CONFIG = genAPIConfig("playerdb");
@@ -33,130 +33,138 @@ const HYPIXEL_KEY = getKey(KEYPATH);
 const HYPIXEL_CONFIG = genAPIConfig("hypixel", HYPIXEL_KEY);
 
 // const CONFIG = {
-//     headers: {
-//         "API-Key": KEY,
-//         "user-agent": `github.com/stinkydubeau/${PROJECTNAME}`,
-//     }
+//         headers: {
+//                 "API-Key": KEY,
+//                 "user-agent": `github.com/stinkydubeau/${PROJECTNAME}`,
+//         }
 // }
 
 APP.use(express.static("./public"));
-APP.use(bodyParser.urlencoded({extended: true}));
+APP.use(bodyParser.urlencoded({ extended: true }));
 APP.use(getUUID);
 APP.use(getStats);
 
-function getKey(path){ // Returns text from 'path'. Creates 'path' if it doesn't exist.
-  var key;
-  var needKeyMsg = '- You have not inserted your API key into apikeys.txt. The app will not work until you do. -'
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    key = data;
-  }
-      catch (err) {
-    try{
-      fs.writeFile(path, needKeyMsg, (err) => {
-        if (err) throw err;
-        console.log(`${path} has been created. Please paste your API key inside.`);
-      });
-    } catch(err){
-      console.error(`Create ${path} and paste your API key inside.`);
+function getKey(path) {
+    // Returns text from 'path'. Creates 'path' if it doesn't exist.
+    var key;
+    var needKeyMsg =
+        "- You have not inserted your API key into apikeys.txt. The app will not work until you do. -";
+    try {
+        const data = fs.readFileSync(path, "utf8");
+        key = data;
+    } catch (err) {
+        try {
+            fs.writeFile(path, needKeyMsg, (err) => {
+                if (err) throw err;
+                console.log(
+                    `${path} has been created. Please paste your API key inside.`
+                );
+            });
+        } catch (err) {
+            console.error(`Create ${path} and paste your API key inside.`);
+        }
     }
-  }
-  return key;
+    return key;
 }
 
-function removeUnderscores(str){
-  var newStr = "";
-  if(!str){
-    console.log("Cannot remove underscores from empty string!");
-    return;
-  }
-  for(var i = 0; i < str.length; i++){
-    if(str[i] == "_"){
-      newStr += " ";
-    }else{
-      newStr += str[i];
+function removeUnderscores(str) {
+    var newStr = "";
+    if (!str) {
+        console.log("Cannot remove underscores from empty string!");
+        return;
     }
-  }
-  return(newStr);
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == "_") {
+            newStr += " ";
+        } else {
+            newStr += str[i];
+        }
+    }
+    return newStr;
 }
 
-function translateJSON(original){ // Converts hypixel's JSON into something easier to work with.
-  var translated = 
-    {
-    username: original.player.displayname,
-    rank: removeUnderscores(original.player.newPackageRank),
-    playtime: original.player.timePlaying,
-    firstPlayed: new Date(original.player.firstLogin),
-    lastPlayed: new Date(original.player.lastLogout),
-  }
-  return(translated);
+function translateJSON(original) {
+    // Converts hypixel's JSON into something easier to work with.
+    var translated = {
+        username: original.player.displayname,
+        rank: removeUnderscores(original.player.newPackageRank),
+        playtime: original.player.timePlaying,
+        firstPlayed: new Date(original.player.firstLogin),
+        lastPlayed: new Date(original.player.lastLogout),
+    };
+    return translated;
 }
 
 function validateUUID(uuid) {
-    const Pattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4|1][0-9a-fA-F]{3}-[8|9|aA|bB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+    const Pattern =
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4|1][0-9a-fA-F]{3}-[8|9|aA|bB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
     if (Pattern.test(uuid)) {
         return uuid; // Minecraft UUID is valid
     } else {
-        console.error("Malformed Minecraft UUID. Attempting to fix or using a default UUID.");
+        console.error(
+            "Malformed Minecraft UUID. Attempting to fix or using a default UUID."
+        );
         return DEFAULTUUID; // Replace with your logic for fixing or default Minecraft UUID
     }
 }
 
 async function getUUID(req, res, next) {
-  if (req.body.username) {
-    try {
-      var url = DBPI + req.body.username;
-      const response = await axios.get(url, PLAYER_DB_CONFIG);
-      req.uuid = response.data.data.player.id;
-    } catch {
-      console.log("Username does not exist! Using default uuid.");
-      req.uuid = DEFAULTUUID;
+    if (req.body.username) {
+        try {
+            var url = DBPI + req.body.username;
+            const response = await axios.get(url, PLAYER_DB_CONFIG);
+            req.uuid = response.data.data.player.id;
+        } catch {
+            console.log("Username does not exist! Using default uuid.");
+            req.uuid = DEFAULTUUID;
+        }
     }
-  }
-  next();
+    next();
 }
 
 async function getStats(req, res, next) {
-  if (HYPIXEL_KEY) {
-    try {
-      var url = HYPI + validateUUID(req.uuid);
-      var response = await axios.get(url, HYPIXEL_CONFIG);
-      if (response.data.player == null) {
-        // TODO: Log a user-error in the site rather than defaulting to Herobrine.
-        console.log(`${req.uuid} has never joined hypixel. Reverting to default UUID.`);
-        url = HYPI + DEFAULTUUID;
-        response = await axios.get(url, HYPIXEL_CONFIG);
-      }
-      req.hypixel = response.data;
+    if (HYPIXEL_KEY) {
+        try {
+            var url = HYPI + validateUUID(req.uuid);
+            var response = await axios.get(url, HYPIXEL_CONFIG);
+            if (response.data.player == null) {
+                // TODO: Log a user-error in the site rather than defaulting to Herobrine.
+                console.log(
+                    `${req.uuid} has never joined hypixel. Reverting to default UUID.`
+                );
+                url = HYPI + DEFAULTUUID;
+                response = await axios.get(url, HYPIXEL_CONFIG);
+            }
+            req.hypixel = response.data;
 
-      //req.hypixel = response.data.player;
-      //return(response)
-    }catch{
-      console.log(`An error occured while looking up hypixel stats.`);
+            //req.hypixel = response.data.player;
+            //return(response)
+        } catch {
+            console.log(`An error occured while looking up hypixel stats.`);
+        }
+    } else {
+        console.log(`You need to define an API key in ${KEYPATH}.`);
     }
-  }else{
-    console.log(`You need to define an API key in ${KEYPATH}.`);
-  }
-  next();
+    next();
 }
 
 APP.get("/", (req, res) => {
-  console.log("Loading home...");
-  res.render("index.ejs");
+    console.log("Loading home...");
+    res.render("index.ejs");
 });
 
 APP.post("/userLookup", async (req, res) => {
-  var response = req.body.username + "'s user ID is " + req.uuid;
-  console.log(response);
-  res.render("index.ejs", {
-    hypixel: translateJSON(req.hypixel)
-  });
-  //res.redirect("/");
+    var response = req.body.username + "'s user ID is " + req.uuid;
+    console.log(response);
+    res.render("index.ejs", {
+        hypixel: translateJSON(req.hypixel),
+    });
+    //res.redirect("/");
 });
 
 APP.listen(PORT, (req, res) => {
-  console.log(`Welcome to ${PROJECTNAME}!`);
-  console.log(`Using API key:   ${HYPIXEL_KEY}`);
-  console.log(`Running on port: ${PORT}`);
+    console.log(`Welcome to ${PROJECTNAME}!`);
+    console.log(`Using API key:     ${HYPIXEL_KEY}`);
+    console.log(`Running on port: ${PORT}`);
 });
--- readme.md
+++ readme.md
@@ -18,21 +18,21 @@ node index.js
 Issues:
 - Log a user-error in the site rather than defaulting to Herobrine.
 - handle nonexistant usernames better.
-    - Currently, it will fallback to DEFAULTUUID if there is no username.
+        - Currently, it will fallback to DEFAULTUUID if there is no username.
 - send user-agent in header when pulling from playerDB
 - disallow illegal characters in username field.
 
 Todo:
 - Show player's last login
 - Show player's account age
-    - Badges based on account age? Similar to Steam.
+        - Badges based on account age? Similar to Steam.
 - Show dropdown to view stats for specific game
-    - Bedwars
-        - Beds broken
-        - Final kills
-        - Leaderboard position (if greater than certain threshold)
-        - Level
+        - Bedwars
+                - Beds broken
+                - Final kills
+                - Leaderboard position (if greater than certain threshold)
+                - Level
 - Show an option to view ALL stats for a given name. Might as well use everything the API gives us. Make it as readable as possible. Start by just dumping the entire JSON, and hone it down after.
 - Cache name searches in our own db to reduce latency and reliance on (slow!) playerdb.
 - Get and display player skin
-    - Get and display player pet beside skin
+        - Get and display player pet beside skin

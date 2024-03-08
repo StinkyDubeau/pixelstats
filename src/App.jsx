027522707b72c093b2714a9a08ea33f641950b0c
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 22:41:52 -0500

    Remove old references

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <div className="mx-auto my-10 max-w-screen-lg rounded-full bg-zinc-100 p-2 text-center shadow-lg">
-                <h1 className="text-3xl tracking-tighter">Pixelstats</h1>
-                <p className="tracking-wider">React version</p>
+            <div className="m-4">
+                <div className="mx-auto max-w-screen-lg rounded-full bg-zinc-100 p-2 text-center shadow-lg">
+                    <h1 className="text-3xl tracking-tighter">Pixelstats</h1>
+                    <p className="tracking-wider">React version</p>
+                </div>
             </div>
         </>
     );

deefdee7290250fbb043ec07efbc59cc8da59426
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 22:28:24 -0500

    Run prettier

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
 # PixelStats React
 
 Hello, this is the React branch of the PixelStats project. It will (hopefully) be merged to main after some refactoring.
-
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="text-center bg-zinc-100 max-w-screen-lg mx-auto my-10 rounded-full shadow-lg p-2">
-        <h1 className="text-3xl tracking-tighter">Pixelstats</h1>
-        <p className="tracking-wider">React version</p>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="mx-auto my-10 max-w-screen-lg rounded-full bg-zinc-100 p-2 text-center shadow-lg">
+                <h1 className="text-3xl tracking-tighter">Pixelstats</h1>
+                <p className="tracking-wider">React version</p>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;

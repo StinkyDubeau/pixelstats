48b33d37a0befa9175af259b9762fa42c5364d23
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-10 09:57:33 -0400

    Tune border

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ function App() {
             <div className="bg-slate-200">
                 <div className="py-4 px-10">
 
-                    <div className="mx-auto my-4 max-w-screen-lg rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border-2 p-2 text-center shadow-lg">
+                    <div className="mx-auto my-4 max-w-screen-lg rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border p-2 text-center shadow-lg">
                         <h1 className="text-3xl tracking-tighter">Pixelstats</h1>
                         <p className="tracking-wider">React version</p>
                     </div>
 
                     <div className="mx-auto flex gap-6 max-w-screen-lg">
 
-                        <div className="fmx-auto my-4  rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border-2 p-2 text-center shadow-lg">
+                        <div className="fmx-auto my-4  rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border p-2 text-center shadow-lg">
                             <div className="px-2 py-4 sm:px-6 sm-py-6">
                                 <p className="text-justify">Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. </p>
                             </div>
                         </div>
 
-                        <div className="mx-auto my-4 rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border-2 p-2 text-center shadow-lg">
+                        <div className="mx-auto my-4 rounded-lg border-t-zinc-500 border-x-zinc-700 border-b-zinc-800 bg-zinc-100 border p-2 text-center shadow-lg">
                             <div className="px-2 py-4 sm:px-6 sm-py-6">
                                 <p className="text-justify">Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. Here is some lorum ipsum text. Blah blah pepsi coke hooray! This is a sentence. </p>
                             </div>

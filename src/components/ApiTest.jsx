7e49a75850b2753abf3756c7b734ae2193442baa
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-18 07:30:46 -0400

    Fix api loading

--- src/components/ApiTest.jsx
+++ src/components/ApiTest.jsx
@@ -7,29 +7,39 @@ export default function ApiTest(props) {
         "https://api.hypixel.net/v2/player?uuid=27166865-269a-4473-baa6-14110cf142bc";
     const key = import.meta.env.VITE_HYPIXEL_API_KEY;
 
-    const [data, setData] = useState();
+    const [data, setData] = useState(null);
 
     useEffect(() => {
         // Fetch data
-        axios.get(url, {
-            headers: {
-                "Api-Key": key
-            }
-        })
-        .then((res) => {
-            console.log(res.data.player);
-            setData(res.data.player);
-        })
-        .catch((err) => console.error(err));
+        axios
+            .get(url, {
+                headers: {
+                    "Api-Key": key,
+                },
+            })
+            .then((res) => {
+                console.log(res.data.player);
+                setData(res.data.player);
+            })
+            .catch((err) => console.error(err));
     }, []);
 
     function createData(data) {
-        console.log(data);
+        console.log(`Data: ${data}`);
 
-        return(<>
-            <p>i am data</p>
-        </>)
+        return (
+            <>
+                <p>i am data</p>
+            </>
+        );
+    }
+    function createLoading() {
+        return (
+            <>
+                <p>Loading data...</p>
+            </>
+        );
     }
 
-    return <h1>{createData()}</h1>;
+    return <h1>{data ? createData(data) : createLoading()}</h1>;
 }
